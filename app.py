d8c723fc5ea13eda318ca9cc373e4178305dfb7a
sean15535 <sean15535@gmail.com>
2024-03-06 17:23:14 +0100

    added the options for the grade calculator

---
+++ app.py
@@ -0,0 +1,5 @@
+grade={ }
+print("Welcome to Honeywell Group of School Grade Calculator")
+while True:
+    print("1. Add Student Grade.\n2. Remove Student Grade.\n3 View Student Grades.\n4. Calculate Average Grade ")
+    
