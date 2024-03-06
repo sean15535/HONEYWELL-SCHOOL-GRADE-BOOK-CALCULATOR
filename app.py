5d19bed0ca8afcef916c71ba28c6f01c4784ce90
sean15535 <sean15535@gmail.com>
2024-03-06 17:32:16 +0100

    did the code to add student name and grade to the gradebook

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 grade={ }
 print("Welcome to Honeywell Group of School Grade Calculator")
 while True:
-    print("1. Add Student Grade.\n2. Remove Student Grade.\n3 View Student Grades.\n4. Calculate Average Grade ")
-    
+    print("1. Add Student Grade.\n2. Remove Student Grade.\n3 View Student Grades.\n4. Calculate Average Grade.\n5. Exit ")
+    response = int(input(">>> "))
+    if response == 1:
+        student_name = str(input("Enter Student Name: "))
+        student_grade = float(input("Enter Student Grade: "))
+        grade[student_name]=student_grade
+        print(f"{student_name} with {student_grade} has been added to the Gradebook")
+    elif response == 2:
