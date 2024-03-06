a9d6a6f3728324fcbc816d70f02ce983fb545dab
sean15535 <sean15535@gmail.com>
2024-03-06 17:40:58 +0100

    added the remove syntax to the code

--- app.py
+++ app.py
@@ -5,7 +5,18 @@ while True:
     response = int(input(">>> "))
     if response == 1:
         student_name = str(input("Enter Student Name: "))
+        if student_name in gradebook:
+            print(f"Warning: {student_name} already exists in the gradebook. Overwriting grade.")
         student_grade = float(input("Enter Student Grade: "))
         grade[student_name]=student_grade
         print(f"{student_name} with {student_grade} has been added to the Gradebook")
     elif response == 2:
+        if not grade:
+            print("Gradebook is empty")
+        else:
+            reply = str(input("Enter Student Name to Remove: "))
+            if reply 
+            grade.pop[reply]
+            print(f"{reply} has been removed from the GradeBook")
+        
+
