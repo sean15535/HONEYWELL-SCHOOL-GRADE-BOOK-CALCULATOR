5ee186cc715cea1bdd9a7438f5b0c2d8f3816f7c
sean15535 <sean15535@gmail.com>
2024-03-08 19:42:46 +0100

    fixed the remove function

--- functions.py
+++ functions.py
@@ -1,9 +1,13 @@
 grade = {}
 
 def grade_calculator():
+    if not grade:
+        print("Grade Book is empty")
+        return
+
     total_grades = sum(grade.values())
     average_grade = total_grades / len(grade)
-    return f"Average Grade: {average_grade}"
+    print(f"Average Grade: {average_grade:.2f}")
 
 def greetings():
     print("1. Add Student Grade.")
@@ -13,20 +17,33 @@ def greetings():
     print("5. Exit")
 
 def add_student():
-    student_name = str(input("Enter Student Name: "))
+    student_name = input("Enter Student Name: ").capitalize()
     if student_name in grade:
         print(f"Warning: {student_name} already exists in the gradebook. Overwriting grade.")
+    try:
         student_grade = float(input("Enter Student Grade: "))
-        grade[student_name] = student_grade
-    return f"{student_name} with grade {student_grade} has been added to the Gradebook"
+    except ValueError:
+        print("Invalid grade input. Please enter a number.")
+        return
+    grade[student_name] = student_grade
+    print(f"{student_name} with grade {student_grade} has been added to the Gradebook")
+
 
 def remove_student_grade():
     if not grade:
         print("Gradebook is empty")
+        return
+    student_name = str(input("Enter Student Name to Remove: ")).capitalize()
+    if student_name in grade:
+        grade.pop(student_name) 
+        print(f"{student_name}'s grade has been removed from the gradebook.")
     else:
-        student_name = str(input("Enter Student Name to Remove: "))
-        if student_name in grade:
-            grade.pop(student_name) 
-            print(f"{student_name}'s grade has been removed from the gradebook.")
-        else:
-            return f"{student_name} does not exist in the gradebook."
+        print(f"{student_name} does not exist in the gradebook.")
+
+def students_grade():
+    if not grade:
+        print("Grade Book is empty")
+        return
+    print("Honeywell Grade Book")
+    for student_name, grades in grade.items():
+        print(f"{student_name}, {grades}")
--- main.py
+++ main.py
@@ -1,29 +1,20 @@
-from functions import grade_calculator, greetings, add_student, remove_student_grade
-grade = {}
+from functions import grade_calculator, greetings, add_student, remove_student_grade, students_grade
 
 print("Welcome to Honeywell Group of School Grade Calculator")
 
 while True:
     greetings()
-    response = int(input(">>> "))
+    response = input(">>> ")
 
-    if response == 1:
+    if response == '1':
         add_student()
-    elif response == 2:
+    elif response == '2':
         remove_student_grade()
-    elif response == 3:
-        if not grade:
-            print("Grade Book is empty")
-        else:
-            print("Honeywell Grade Book")
-            for student_name, grades in grade.items():
-                print(f"{student_name}, {grades}")
-    elif response == 4:
-        if not grade:
-            print("Grade Book is empty")
-        else:
-            grade_calculator()
-    elif response == 5:
+    elif response == '3':
+        students_grade()
+    elif response == '4':
+        grade_calculator()
+    elif response == '5':
         print("Exiting App")
         break
     else:
